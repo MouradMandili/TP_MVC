09ae8ff340c732c06813622f3ee41a02b7921aaa
MouradMandili <tpa.nord@gmail.com>
2021-05-27 23:59:23 +0200

    requete insert BDD ok preparation structure MVC ok

---
+++ MODEL/User.php
@@ -0,0 +1,19 @@
+<?php
+
+class User{
+
+    private _email;
+    private _mdp;
+
+
+
+    //fct lire une ligne base de donnée par rapport email mdp
+
+    //fct sauvegarde les donnée passé en param
+
+    //fct recuperer plusieurs ligne dans la BDD
+
+
+
+}
+?>
